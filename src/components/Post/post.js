b4108a3da03eb3c122c1ff2d96365b78f64e90af
risav-sarkar <risavabir2001@gmail.com>
2022-02-15 20:39:41 +0530

    feat: created post component

--- src/components/Post/post.js
+++ src/components/Post/post.js
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import { AboutPic, UserPic } from "../assets";
+import PostModal from "./postModal";
+
 const Post = () => {
-  return <div className="post"></div>;
+  const [modal, setModal] = useState(0);
+  return (
+    <div className="postContainer">
+      <div className="postHeader">
+        <div className="imageContainer">
+          <img src={UserPic} alt="postImage" />
+        </div>
+        <h2>Name</h2>
+      </div>
+      <div className="postTitle">
+        <h4>Lorem ipsum blah blah blah</h4>
+      </div>
+      <div className="postImage" onClick={() => setModal(1)}>
+        <img src={AboutPic} alt="postImage" />
+        <div className="text">
+          <h2>More Details!</h2>
+        </div>
+      </div>
+      {modal === 1 ? <PostModal setModal={setModal} /> : null}
+    </div>
+  );
 };
 
 export default Post;
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,10 +1,13 @@
 import CreatePost from "../components/CreatePost/createPost";
+import Post from "../components/Post/post";
 
 const User = () => {
   return (
     <div className="userHomeLayout">
       <div className="userHomeContent">
         <CreatePost />
+        <Post />
+        <Post />
       </div>
     </div>
   );
